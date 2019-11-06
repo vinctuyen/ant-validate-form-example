6aaf5caa696d9fe071a1c7a785e07dc2393743ae
Tuyen Nguyen (MOR) <tuyen.nguyenvan@mor.com.vn>
2019-11-06 13:26:03 +0700

    add validate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { Form, Input, Button, Row } from "antd";
+import "antd/dist/antd.css";
 
-function App() {
+function AppForm(props) {
+  const [phone, setPhone] = useState('')
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 2 }
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 22 }
+    }
+  };
+  const { getFieldDecorator } = props.form;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row className="App" style={{textAlign:'center', padding: '20%' }}>
+      <h1>Form validate</h1>
+      <Form  {...formItemLayout}>
+        <Form.Item label="E-mail">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Phone">
+          {getFieldDecorator("phone", {
+            rules: [
+              {
+                max:10,
+                message: "Phone number have 10 number"
+              },
+              {
+                min:10,
+                message: "Phone number have 10 number"
+              },
+              {
+                required: true,
+                message: "Please input your phone"
+              }
+            ]
+          })(<Input type="number" value={phone} onChange={e => setPhone(e.target.value)} />)}
+        </Form.Item>
+        <Form.Item label="pas">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="E-mail">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+      </Form>
+      <Button type="primary">Submit</Button>
+    </Row>
   );
 }
+
+const App = Form.create({ name: "register" })(AppForm);
 
 export default App;
